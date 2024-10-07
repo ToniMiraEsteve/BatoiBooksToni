5ef624efceafda6957a2cd9520c7c46bda218cd6
Toni Mira Esteve <tonimiraesteve@gmail.com>
2024-10-07 16:12:20 +0200

    BatoiBooks base

---
+++ main.js
@@ -0,0 +1,15 @@
+import viteLogo from '/public/logoBatoi.png';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <h1>
+      BatoiBooks
+    </h1>
+    <p class="read-the-docs">
+      Abre la consola para ver el resultado
+  </div>
+`
+
