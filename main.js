bb9b5356e0340446891cabd5811ff55a0749baf6
Toni Mira Esteve <tonimiraesteve@gmail.com>
2024-10-08 15:04:18 +0200

    BatoiBooks arrays

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+
 import viteLogo from '/public/logoBatoi.png';
+import { 
+  getBookById, 
+  getBookIndexById, 
+  bookExists, 
+  booksFromUser, 
+  booksFromModule, 
+  booksCheeperThan, 
+  booksWithStatus, 
+  averagePriceOfBooks, 
+  booksOfTypeNotes, 
+  booksNotSold, 
+  incrementPriceOfbooks, 
+  getUserById, 
+  getUserIndexById, 
+  getUserByNickName, 
+  getModuleByCode 
+} from '/srv/funciones.js';
+
+import { libros } from '/srv/services/datos.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -13,3 +33,15 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
+const librosUsuario4 = booksFromUser(libros, 4);
+console.log("Libros del usuario 4:");
+console.log(librosUsuario4);
+
+const librosModulo5021 = booksFromModule(libros, 5021);
+const librosModulo5021Good = booksWithStatus(librosModulo5021, 'Good');
+console.log("Libros del módulo 5021 en buen estado:");
+console.log(librosModulo5021Good);
+
+const librosPrecioIncrementado = incrementPriceOfbooks(libros, 10);
+console.log("Libros con precio incrementado en un 10%:");
+console.log(librosPrecioIncrementado);
